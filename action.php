7b55df36a3ea2776a6ed6c4a496213d7d4a46343
taramozart <taramozart666@gmail.com>
2022-10-07 00:50:43 +0700

    pemweb

---
+++ action.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Detail Produk</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class = "grid">
+    <h1 align="center">Detail Informasi Produk</h1>
+    <table align="center">
+        <tr>
+            <td>No</td>
+            <td>Jenis Produk</td>
+            <td>Nama Produk</td>
+            <td>Jumlah Produk</td>
+            <td>Harga satuan</td>
+        </tr>
+        <tr>
+            <td>1.</td>
+            <td>Makanan</td>
+            <td>Tango</td>
+            <td>50 pcs</td>
+            <td>5000</td>
+        </tr>
+        <tr>
+            <td>2.</td>
+            <td>Minuman</td>
+            <td>Teh botol</td>
+            <td>100 botol</td>
+            <td>3000</td>
+        </tr>
+        <tr>
+            <td>3.</td>
+            <td>Minuman</td>
+            <td>Milk Tea</td>
+            <td>75 botol</td>
+            <td>8000</td>
+        </tr>
+        <tr>
+            <td>4.</td>
+            <td>Obat</td>
+            <td>Panadol</td>
+            <td>100 pcs</td>
+            <td>2500</td>
+        </tr>
+        <tr>
+            <td>5.</td>
+            <td>Alat Tulis</td>
+            <td>Bintang Obor</td>
+            <td>10 pack</td>
+            <td>4500</td>
+        </tr>
+    </table>
+    </div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Product Login</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<form  align="center" action="action.php" method="get">
+    <div class = "login">
+        <h2 >Masuk ke detail produk!</h2>
+             <label> Username </label><br>
+             <input type="text" name ="name"><br>
+             <label> ID Customer </label><br>
+             <input type="text" name ="id"><br><br>
+             <input type="submit" value="Login">
+    </div>
+    </form>
+ 
+</body>
+</html>
